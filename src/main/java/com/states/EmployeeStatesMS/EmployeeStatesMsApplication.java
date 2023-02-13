f43b5641574f8905018819300814516312bccc02
Anazmy <abdallah.mohyeldin@vodafone.com>
2023-02-13 11:39:31 +0200

    -Initial Commit

---
+++ src/main/java/com/states/EmployeeStatesMS/EmployeeStatesMsApplication.java
@@ -0,0 +1,13 @@
+package com.states.EmployeeStatesMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeStatesMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeStatesMsApplication.class, args);
+	}
+
+}
